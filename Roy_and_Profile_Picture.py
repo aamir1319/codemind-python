36f415692030f87aa9b1a3cfdaae81d4a2c60f20
aamir1319 <108738138+aamir1319@users.noreply.github.com>
2022-09-04 16:13:56 +0530

    l=int(input()) n=int(input()) s=l*l for i in range(n):     w,h=map(int,input().split())     if(w<l) or (h<l):         print("UPLOAD ANOTHER")     elif(w==h):         print("ACCEPTED")     else:         print("CROP IT")

---
+++ Roy_and_Profile_Picture.py
@@ -0,0 +1,12 @@
+l=int(input())
+n=int(input())
+s=l*l
+for i in range(n):
+    w,h=map(int,input().split())
+    if(w<l) or (h<l):
+        print("UPLOAD ANOTHER")
+    elif(w==h):
+        print("ACCEPTED")
+    else:
+        print("CROP IT")
+    
