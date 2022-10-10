7c5ee01dcffccc5fde3f8df9a15eaad41449111d
aamir1319 <108738138+aamir1319@users.noreply.github.com>
2022-10-10 15:20:22 +0530

    def prime(n):     if n==1:         return False     for i in range(2,int(n**.5)+1):         if n%i==0:             return False     return True
    t=int(input())
    for i in range(t):
        n=int(input())
        if prime(n):
            print(n)
        else:
            after=n+1
            before=n-1
            while after:
                if prime(after):
                    break
                after+=1
            while before:
                if prime(before):
                    break
                before-=1
            diff=after-n
            diff1=n-before
            if diff>diff1:
                print(before)
            elif diff==diff1:
                print(before)
            else:
                print(after)

--- Nearest_Prime.py
+++ Nearest_Prime.py
@@ -1,32 +1,32 @@
 def prime(n):
     if n==1:
         return False
-    for i in range(2,int(n**0.5)+1):
+    for i in range(2,int(n**.5)+1):
         if n%i==0:
             return False
     return True
-
-t = int(input())
+    
+t=int(input())
 for i in range(t):
-    n = int(input())
-    after = n
-    before =n
-    while after:
-        if prime(after):
-            break
-        after+=1
-    
-    while before:
-        if prime(before):
-            break
-        before-=1
-    
-    daf = after-n
-    dbef  = n-before
-    
-    if daf==dbef:
-        print(before)
-    elif daf>dbef:
-        print(before)
+    n=int(input())
+    if prime(n):
+        print(n)
     else:
-        print(after)
+        after=n+1
+        before=n-1
+        while after:
+            if prime(after):
+                break
+            after+=1
+        while before:
+            if prime(before):
+                break
+            before-=1
+        diff=after-n
+        diff1=n-before
+        if diff>diff1:
+            print(before)
+        elif diff==diff1:
+            print(before)
+        else:
+            print(after)
