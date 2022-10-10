e3a8816d04821b74ecda0c6694ecbda323165a51
aamir1319 <108738138+aamir1319@users.noreply.github.com>
2022-10-10 16:24:53 +0530

    def prime(n):     if n==1:         return False     for i in range(2,int(n**0.5)+1):         if n%i==0:             return False     return True
    a=int(input())
    b = int(input())
    
    for i in range(a,b+1):
        if prime(i):
            print(i)

--- Generate_Prime_Numbers_In_A_Interval_.py
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -1,13 +1,14 @@
 def prime(n):
-    if n==0 or n==1:
+    if n==1:
         return False
-    for i in range(2,int(n**.5)+1):
+    for i in range(2,int(n**0.5)+1):
         if n%i==0:
             return False
     return True
-n1=int(input())
-n2=int(input())
-for i in range(n1,n2):
+
+a=int(input())
+b = int(input())
+
+for i in range(a,b+1):
     if prime(i):
         print(i)
-        
