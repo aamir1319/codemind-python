08549e306deaa78b2080c3dec28a4d3e5b8d699d
aamir1319 <108738138+aamir1319@users.noreply.github.com>
2022-10-10 17:21:52 +0530

    def prime(n):     if n==1:         return False     for i in range(2,int(n**.5)+1):         if n%i==0:             return False     return True
    def reverse(n):
        temp=n
        rev=0
        while temp:
            rev=rev*10+temp%10
            temp//=10
        return rev
    
    
    p=int(input())
    if prime(p) and prime(reverse(p)):
            print("circular prime")
    elif prime(p):
        print("prime but not a circular prime")
    else:
        print("not prime")

--- circular_prime.py
+++ circular_prime.py
@@ -1,25 +1,25 @@
 def prime(n):
     if n==1:
         return False
-    for i in range(2,int(n**0.5)+1):
+    for i in range(2,int(n**.5)+1):
         if n%i==0:
             return False
     return True
 
-def REV(n):
-    temp = n
-    rev = 0
+def reverse(n):
+    temp=n
+    rev=0
     while temp:
-        rev = rev*10 +temp%10
+        rev=rev*10+temp%10
         temp//=10
     return rev
-    
-n = int(input())
-rn = REV(n)
 
-if prime(n) and prime(rn):
-    print('circular prime')
-elif prime(n):
-    print('prime but not a circular prime')
+
+p=int(input())
+if prime(p) and prime(reverse(p)):
+        print("circular prime")
+elif prime(p):
+    print("prime but not a circular prime")
 else:
-    print('not prime')
+    print("not prime")
+        
