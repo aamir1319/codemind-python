36d6ac018be7c8c1c5c33df123659489f32205d1
aamir1319 <108738138+aamir1319@users.noreply.github.com>
2022-10-10 17:43:42 +0530

    def selfdiv(n):     temp=n     while temp:         a=temp%10         if a==0:             return False         elif n%a!=0:             return False         temp//=10     return True n1=int(input()) n2=(int(input())) for i in range(n1,n2+1):     if selfdiv(i):         print(i,end=" ")

--- Self_Dividing_Numbers.py
+++ Self_Dividing_Numbers.py
@@ -1,17 +1,15 @@
-def sdn(n):
-    temp =n
+def selfdiv(n):
+    temp=n
     while temp:
-        z = temp%10
-        if z==0:
+        a=temp%10
+        if a==0:
             return False
-        elif n%(z)!=0:
+        elif n%a!=0:
             return False
         temp//=10
     return True
-
-a = int(input())
-b = int(input())
-
-for i in range(a,b+1):
-    if sdn(i):
-        print(i,end=' ')
+n1=int(input())
+n2=(int(input()))
+for i in range(n1,n2+1):
+    if selfdiv(i):
+        print(i,end=" ")
