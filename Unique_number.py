16c1a5a748fb1d4912a7ed8511e9daad365c7543
aamir1319 <108738138+aamir1319@users.noreply.github.com>
2022-10-10 15:10:36 +0530

    n=list(input()) # print(n) for i in n:     # print(i)     if n.count(i)!=1:         # print(i)         print("Not Unique Number")         break
    else:
        print("Unique Number")

--- Unique_number.py
+++ Unique_number.py
@@ -1,17 +1,11 @@
-a = list(map(int,input()))
-
-for i in range(len(a)):
-    flag=1
-    for j in range(len(a)):
-        if i==j:
-            continue
-        else:
-            if a[i]==a[j]:
-                flag=0
-                break
-    if flag==0:
+n=list(input())
+# print(n)
+for i in n:
+    # print(i)
+    if n.count(i)!=1:
+        # print(i)
+        print("Not Unique Number")
         break
-if flag==1:
-    print('Unique Number')
+        
 else:
-    print('Not Unique Number')
+    print("Unique Number")
