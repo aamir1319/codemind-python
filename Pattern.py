b67d97f375afcad75e0e08f12fd094403f160ac5
aamir1319 <108738138+aamir1319@users.noreply.github.com>
2022-10-10 17:43:25 +0530

    n = int(input())
    nj = n
    
    nk = 1
    
    nl = nk
    for i in range(n):
        for j in range(nj-1):
            print(end=' ')
        nj-=1
        for k in range(1,nk+1):
            print(k,end='')
        nk+=1
        for l in range(nl-1,0,-1):
            print(l,end='')
        nl+=1
        print()

--- Pattern.py
+++ Pattern.py
@@ -1,6 +1,18 @@
 n = int(input())
 
-for i in range(1,n+1):
-    for j in range(n):
-        print(i,end=' ')
+nj = n
+
+nk = 1
+
+nl = nk
+for i in range(n):
+    for j in range(nj-1):
+        print(end=' ')
+    nj-=1
+    for k in range(1,nk+1):
+        print(k,end='')
+    nk+=1
+    for l in range(nl-1,0,-1):
+        print(l,end='')
+    nl+=1
     print()
