88bd9d940ae1f109ab34a1accfb871754218c1c2
aamir1319 <108738138+aamir1319@users.noreply.github.com>
2022-10-10 15:06:41 +0530

    n=input() k=int(n)*int(n) for i in n:     if n in str(k):         print('Automorphic Number')         break else:     print('Not an Automorphic Number')

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,6 +1,8 @@
-a = int(input())
-
-if str(a) in str(a*a):
-    print('Automorphic Number')
+n=input()
+k=int(n)*int(n)
+for i in n:
+    if n in str(k):
+        print('Automorphic Number')
+        break
 else:
     print('Not an Automorphic Number')
