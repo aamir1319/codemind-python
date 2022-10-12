9003d9a25108427ee5fd6ebd7783dcca0094c4e5
aamir1319 <108738138+aamir1319@users.noreply.github.com>
2022-10-12 23:26:10 +0530

    n = int(input()) x = list(map(int,input().split()))
    e=[]
    o = []
    for i in x:
        if i%2==0:
            e.append(i)
        else:
            o.append(i)
    for i in range(max(len(e),len(o))):
        if len(e)==0:
            print(o[0],end=' ')
            o.remove(o[0])
        elif len(o)==0:
            print(e[0],end=' ')
            e.remove(e[0])
        elif len(e)!=0 and len(o)!=0:
            print(e[0],o[0],end=' ')
            e.remove(e[0])
            o.remove(o[0])
        else:
            break
    if n%2!=0:
        print(0)

---
+++ even_odd_pairs.py
@@ -0,0 +1,25 @@
+n = int(input())
+x = list(map(int,input().split()))
+
+e=[]
+o = []
+for i in x:
+    if i%2==0:
+        e.append(i)
+    else:
+        o.append(i)
+for i in range(max(len(e),len(o))):
+    if len(e)==0:
+        print(o[0],end=' ')
+        o.remove(o[0])
+    elif len(o)==0:
+        print(e[0],end=' ')
+        e.remove(e[0])
+    elif len(e)!=0 and len(o)!=0:
+        print(e[0],o[0],end=' ')
+        e.remove(e[0])
+        o.remove(o[0])
+    else:
+        break
+if n%2!=0:
+    print(0)
