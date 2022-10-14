1733ba4afd23cf8270d32a26956ccada051f8311
aamir1319 <108738138+aamir1319@users.noreply.github.com>
2022-10-14 13:50:41 +0530

    s = input().split() v = 'aeiou' mc = 0 for i in s[0]:     if i in v:         mc+=1
    for i in s:
        c=0
        for j in i:
            if j in v:
                c+=1
        if mc>c:
            mc = c
    wc=0
    for i in s:
        c=0
        for j in i:
            if j in v:
                c+=1
        if mc==c:
            wc+=1
    print(wc)

---
+++ minimum_vowel_count_words.py
@@ -0,0 +1,23 @@
+s = input().split()
+v = 'aeiou'
+mc = 0
+for i in s[0]:
+    if i in v:
+        mc+=1
+
+for i in s:
+    c=0
+    for j in i:
+        if j in v:
+            c+=1
+    if mc>c:
+        mc = c
+wc=0
+for i in s:
+    c=0
+    for j in i:
+        if j in v:
+            c+=1
+    if mc==c:
+        wc+=1
+print(wc)
